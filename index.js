d59848ac35355257774399f87dd8d4edd069b7fb
pbsathvik10 <pbsathvik10@gmail.com>
2026-02-22 12:02:15 +0530

    added css and counter

--- index.js
+++ index.js
@@ -13,15 +13,17 @@ function addTask(){
 function displayTasks(){
     let conEle=document.getElementById("con");
     let trs="";
+    let count=0;
     for(let i in tasks){
+      
         trs+=`<tr>
-                <td>${tasks[i]}</td>
+                <td>  ${++count}. ${tasks[i]}</td>
                 <td><button onclick="editTask(${i})">edit</button></td>
                 <td><button  onclick="deleteTask(${i})">delete</button></td>
             </tr>`
        
     }
-    let table=`<table border="1px">
+    let table=`<table border="0px">
                     ${trs}
                     </table>`
      conEle.innerHTML=table;

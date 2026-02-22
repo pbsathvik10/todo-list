e390a41ed7691e9e3eebbc2ea6cfa0ed05bbef90
pbsathvik10 <pbsathvik10@gmail.com>
2026-02-22 11:43:02 +0530

    data as table format

--- index.js
+++ index.js
@@ -8,28 +8,56 @@ function addTask(){
     displayTasks();
 }
 
+
+
 function displayTasks(){
-        let conEle=document.getElementById("con");
-        let ol=document.createElement("ol");
-         conEle.innerHTML = "";  
-        tasks.forEach((task,index )=> {
-                
-                let li=document.createElement("li");
-
-                li.innerText=task;
-                 let deletebtn=document.createElement("button");
-                 deletebtn.onclick=()=>{deleteTask(index);};
-
-              li.appendChild(deletebtn);
-        ol.appendChild(li);
-        });
-        conEle.appendChild(ol);
+    let conEle=document.getElementById("con");
+    let trs="";
+    for(let i in tasks){
+        trs+=`<tr>
+                <td>${tasks[i]}</td>
+                <td><button onclick="editTask(${i})">edit</button></td>
+                <td><button  onclick="deleteTask(${i})">delete</button></td>
+            </tr>`
+       
+    }
+    let table=`<table border="1px">
+                    ${trs}
+                    </table>`
+     conEle.innerHTML=table;
 }
 
+
+
+// function displayTasks(){
+//         let conEle=document.getElementById("con");
+//         let ol=document.createElement("ol");
+//          conEle.innerHTML = "";  
+//         tasks.forEach((task,index )=> {
+                
+//                 let li=document.createElement("li");
+
+//                 li.innerText=task;
+//                  let deletebtn=document.createElement("button");
+//                  deletebtn.onclick=()=>{deleteTask(index);};
+
+//               li.appendChild(deletebtn);
+//         ol.appendChild(li);
+//         });
+//         conEle.appendChild(ol);
+// }
+
+
+
+function editTask(index){
+   let task= prompt("enter task");
+     tasks[index]=task;
+     displayTasks();
+}
 function deleteTask(index){
     tasks.splice(index,1);
     displayTasks();
-
-
 }
+
+
 
